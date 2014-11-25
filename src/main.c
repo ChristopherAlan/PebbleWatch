f1bf4d6434fe74b7979790e9ee35f58f8e754769
Christopher Alan <chris@christopheralan.us>
2014-11-24 21:21:30 -0800

    added day of the week

--- src/main.c
+++ src/main.c
@@ -1,9 +1,13 @@
 #include <pebble.h>
   
 static Window *s_main_window;
+
 static TextLayer *s_time_layer;
+static TextLayer *s_day_layer;
 
 static GFont s_time_font;
+static GFont s_day_font;
+
 static BitmapLayer *s_background_layer;
 static GBitmap *s_background_bitmap;
 
@@ -13,43 +17,65 @@ static void update_time() {
   struct tm *tick_time = localtime(&temp);
 
   // Create a long-lived buffer
-  static char buffer[] = "00:00";
+  static char time_buffer[] = "00:00";
+  static char day_buffer[] = "XXX";
 
   // Write the current hours and minutes into the buffer
   if(clock_is_24h_style() == true) {
     // Use 24 hour format
-    strftime(buffer, sizeof("00:00"), "%H:%M", tick_time);
+    strftime(time_buffer, sizeof("00:00"), "%H:%M", tick_time);
   } else {
     // Use 12 hour format
-    strftime(buffer, sizeof("00:00"), "%I:%M", tick_time);
+    strftime(time_buffer, sizeof("00:00"), "%I:%M", tick_time);
   }
 
+  // Kludge to handle lack of non-padded hour format string
+  // for twelve hour clock.
+  if (!clock_is_24h_style() && (time_buffer[0] == '0')) {
+    memmove(time_buffer, &time_buffer[1], sizeof(time_buffer) - 1);
+  }
+  
   // Display this time on the TextLayer
-  text_layer_set_text(s_time_layer, buffer);
+  text_layer_set_text(s_time_layer, time_buffer);
+  
+  // Write day of the week into buffer
+  strftime(day_buffer, sizeof("XXX"), "%a", tick_time);
+  text_layer_set_text(s_day_layer, day_buffer);
 }
 
 static void main_window_load(Window *window) {
-// Create GBitmap, then set to created BitmapLayer
-s_background_bitmap = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_SOLAR_BACKGROUND);
-s_background_layer = bitmap_layer_create(GRect(0, 0, 144, 168));
-bitmap_layer_set_bitmap(s_background_layer, s_background_bitmap);
-layer_add_child(window_get_root_layer(window), bitmap_layer_get_layer(s_background_layer));
+  // Create GBitmap, then set to created BitmapLayer
+  s_background_bitmap = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_SOLAR_BACKGROUND);
+  s_background_layer = bitmap_layer_create(GRect(0, 0, 144, 168));
+  bitmap_layer_set_bitmap(s_background_layer, s_background_bitmap);
+  layer_add_child(window_get_root_layer(window), bitmap_layer_get_layer(s_background_layer));
   
-// Create time TextLayer
-  s_time_layer = text_layer_create(GRect(0, 12, 144, 50));
+  // Create time TextLayer
+  s_time_layer = text_layer_create(GRect(0, 8, 142, 50));
   text_layer_set_background_color(s_time_layer, GColorClear);
   text_layer_set_text_color(s_time_layer, GColorWhite);
   text_layer_set_text(s_time_layer, "00:00");
+  
+  // Create day TextLayer
+  s_day_layer = text_layer_create(GRect(4, 0, 144, 20));
+  text_layer_set_background_color(s_day_layer, GColorClear);
+  text_layer_set_text_color(s_day_layer, GColorWhite);
+  text_layer_set_text(s_day_layer, "XXX");
 
 // Create GFont
-  s_time_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_SANFRAN_D_REG_48));
+  s_time_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_SANFRAN_ULIGHT_48));
+  s_day_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_SANFRAN_D_REG_14));
 
 // Apply to TextLayer
   text_layer_set_font(s_time_layer, s_time_font); 
   text_layer_set_text_alignment(s_time_layer, GTextAlignmentRight);
+  
+  text_layer_set_font(s_day_layer, s_day_font); 
+  text_layer_set_text_alignment(s_day_layer, GTextAlignmentLeft);
 
 // Add it as a child layer to the Window's root layer
   layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_time_layer));
+  layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_day_layer));
   
   // Make sure the time is displayed from the start
   update_time();
@@ -58,6 +84,7 @@ layer_add_child(window_get_root_layer(window), bitmap_layer_get_layer(s_backgrou
 static void main_window_unload(Window *window) {
  // Unload GFont
     fonts_unload_custom_font(s_time_font);
+    fonts_unload_custom_font(s_day_font);
   
  // Destroy GBitmap
     gbitmap_destroy(s_background_bitmap);
@@ -67,10 +94,12 @@ static void main_window_unload(Window *window) {
   
   // Destroy TextLayer
     text_layer_destroy(s_time_layer);
+    text_layer_destroy(s_day_layer);
 }
 
 static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
   update_time();
+  
 }
 
 static void init() {
